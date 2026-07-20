269782f732496e1d7ee34e82b04972e1052bc074
Francois0510 <franbure0510@gmail.com>
2026-07-20 12:43:48 -0500

    fix: revertir DELETE enrollment solo para admin, quitar boton retirarse de React

--- src/routes/enrollment.routes.js
+++ src/routes/enrollment.routes.js
@@ -18,6 +18,6 @@ router.get('/all', protect, authorize('admin', 'teacher'), getEnrollments);
 router.get('/:id', protect, getEnrollmentById);
 router.post('/', protect, authorize('student'), createEnrollment);
 router.put('/:id', protect, authorize('admin', 'teacher'), updateEnrollment);
-router.delete('/:id', protect, deleteEnrollment);
+router.delete('/:id', protect, authorize('admin'), deleteEnrollment);
 
 module.exports = router;
